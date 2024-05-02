40aeeb4ab6e921048be92b5d26aaa46253641033
quentin shin <tvy6kv@virginia.edu>
2024-05-02 17:51:54 +0000

    bash and python files for lab 8

---
+++ lab8/lab8bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# retrieve data from url
+curl -s "https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85" > aviation.json
+
+# parse data, pull receiptTime values and output first six values
+jq -r '.[] | .receiptTime' aviation.json | head -n 6
+
---
+++ lab8/lab8py.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import json
+
+# Open json file and load data
+with open('data/schacon.repos.json', 'r') as file:
+    repos = json.load(file)
+
+# Open csv file
+with open('chacon.csv', 'w') as csv_file:
+    for repo in repos[:5]: # first 5
+        name = repo['name']
+        html_url = repo['html_url']
+        updated_at = repo['updated_at']
+        visibility = repo['visibility']
+        
+        # formatting and write the line
+        line = f"{name},{html_url},{updated_at},{visibility}\n"
+        csv_file.write(line)
